7d8ab386e366e079f02b3f6e74c9b615d12b08aa
tanu2026 <tanushaj2015@gmail.com>
2026-05-24 14:00:20 +0530

    addedhost

--- app.py
+++ app.py
@@ -95,4 +95,4 @@ def order():
 # ── Run ───────────────────────────────────────────────────────────────────────
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0",debug=True,port=5000)
